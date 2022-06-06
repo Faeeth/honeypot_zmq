b49dc4392f5b104ad6567033efdd6571d70a59d4
Faeeth <tellig4x@gmail.com>
2022-06-06 17:57:11 +0200

    Add ZMQ_server.py

---
+++ ZMQ_server.py
@@ -0,0 +1,43 @@
+import zmq
+import zmq.auth
+import asyncio
+import zmq.asyncio
+import zlib
+
+certs_dirname = "certs"
+certs_name = "honeypot-cert"
+url = "tcp://*:5000"
+
+def auth_service():
+    auth = zmq.auth.Authenticator(ctx)
+    auth.start()
+    auth.configure_curve(location=f"./{certs_dirname}")
+
+async def sub(ctx):
+    auth_service()
+    print("-- SUBSCRIBER SERVER --")
+    socket = ctx.socket(zmq.SUB)
+
+    # Subscribe on all topics.
+    socket.setsockopt(zmq.SUBSCRIBE, b"")
+
+    # Load server keys.
+    public_key, secret_key = zmq.auth.load_certificate(f"./{certs_dirname}/{certs_name}.key_secret")
+
+    socket.curve_publickey = public_key
+    socket.curve_secretkey = secret_key
+    socket.curve_server = True
+
+    socket.bind(url)
+
+    while True:
+        data = await socket.recv()
+        try:
+            decompressed_data = zlib.decompress(data).decode("utf-8")
+            print(decompressed_data)
+        except Exception as e:
+            print("error : ", e)
+
+if __name__ == "__main__":
+    ctx = zmq.asyncio.Context()
+    asyncio.run(sub(ctx))
