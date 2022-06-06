619bfd66c7026c8d27f0affec75121ac3b7d03a9
Faeeth <tellig4x@gmail.com>
2022-06-06 18:44:09 +0200

    Add class PUB

--- ZMQ_client.py
+++ ZMQ_client.py
@@ -1,36 +1,39 @@
 import time
-
 import zmq
 import zmq.auth
 import zlib
+import config
 
-certs_dirname = "certs"
-certs_name = "honeypot-cert"
-url = "tcp://127.0.0.1:5000"
+class zmq_pub():
+    def __init__(self):
+        self.ctx = zmq.asyncio.Context()
+        self.launch()
 
-def auth_service():
-    auth = zmq.auth.Authenticator(ctx)
-    auth.start()
-    auth.configure_curve(location=f"./{certs_dirname}")
 
-def pub(ctx):
-    print("-- PUBLISHER CLIENT --")
-    socket = ctx.socket(zmq.PUB)
+    def auth_service(self):
+        self.auth = zmq.auth.Authenticator(self.ctx)
+        self.auth.start()
+        self.auth.configure_curve(location=f"./{config.certs_dirname}")
 
-    # Load client keys.
-    public_key, secret_key = zmq.auth.load_certificate(f"./{certs_dirname}/{certs_name}.key_secret")
-    socket.curve_publickey = public_key
-    socket.curve_secretkey = secret_key
+    def launch(self):
+        self.auth_service()
+        print("-- PUBLISHER CLIENT --")
+        self.socket = self.ctx.socket(zmq.PUB)
 
-    # Load server public key.
-    server_public_key, _ = zmq.auth.load_certificate(f"./{certs_dirname}/{certs_name}.key")
-    socket.curve_serverkey = server_public_key
+        # Load client keys.
+        public_key, secret_key = zmq.auth.load_certificate(f"./{config.certs_dirname}/{config.certs_name}.key_secret")
+        self.socket.curve_publickey = public_key
+        self.socket.curve_secretkey = secret_key
 
-    socket.connect(url)
-    while True:
-        socket.send(zlib.compress(bytes(f"test",encoding="utf-8")))
-        time.sleep(1)
+        # Load server public key.
+        server_public_key, _ = zmq.auth.load_certificate(f"./{config.certs_dirname}/{config.certs_name}.key")
+        self.socket.curve_serverkey = server_public_key
 
-if __name__ == "__main__":
-    ctx = zmq.asyncio.Context()
-    pub(ctx)
+        # Connect to server
+        self.socket.connect(config.url)
+
+    def send(self,data):
+        try:
+            self.socket.send(zlib.compress(bytes(f"{data}", encoding="utf-8")))
+        except Exception as e:
+            print(f"[Error] : {e}")
